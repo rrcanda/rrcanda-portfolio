24cba8d7c51dd53102cc6fea8b0bb3432dc274ad
rrcanda <rrcanda@users.noreply.github.com>
2025-04-27 10:23:12 +0800

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed = new Typed(".typing", {
+    strings: ["","Computer Engineer", "Virtual Assistant", "Frontend Developer", "Math Tutor"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
